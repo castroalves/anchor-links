6d1a3a99474874d301aeb595b64479d7faa41525
Cadu de Castro Alves <cadu@bluefactory.com.br>
2018-02-24 14:33:02 +0000

    Added copy to clipboard, compatibility with desktop and Medium mobile app and other improvements

--- medium-anchor.js
+++ medium-anchor.js
@@ -1,23 +1,42 @@
-var currentSelection = getCurrentSelection();
-if( currentSelection !== '' ) {
-	prompt( 'Copy the following URL:', currentSelection.url );	
+var anchorText = getAnchor();
+if( anchorText !== '' ) {
+	copyTextToClipboard( anchorText );
+	alert('Copied to your clipboard!');
 } else {
-	alert( 'Nothing was selected. Try again!' );
+	alert('Ooops, something went wrong. Try again.');
 }
 
-function getCurrentSelection() {
-	var selectedText = '';
+function getAnchor() {
+	var anchorText = '';
 	if ( window.getSelection ) {
 		if( window.getSelection().toString() !== '' ) {
-			var postArticle = document.querySelector('.postArticle-content');
-			var postId = postArticle.dataset.postId;
-			var baseUrl = 'https://medium.com/p';
-			var elementId = window.getSelection().anchorNode.parentNode.id;
-			selectedText = {
-				text: window.getSelection().anchorNode.parentNode.textContent,
-				url: baseUrl + '/' + postId + '#' + elementId
-			};
+			var baseUrl = window.location.href;
+			var elementId = window.getSelection().anchorNode.parentNode.attributes.name.value;
+			anchorText = '#' + elementId;
 		}
 	}
-	return selectedText;
+	return anchorText;
+}
+
+function copyTextToClipboard(text) {
+	var input = document.createElement('input');
+	input.style.width = '2em';
+	input.style.height = '2em';
+	input.style.padding = 0;
+	input.style.border = 'none';
+	input.style.outline = 'none';
+	input.style.boxShadow = 'none';
+	input.style.background = 'transparent';
+	input.value = text;
+	document.body.appendChild(input);
+	input.select();
+	try {
+		var successful = document.execCommand('copy');
+		var msg = successful ? 'successful' : 'unsuccessful';
+		// console.log('Copying text command was ' + msg);
+		// console.log('Text to clipboard: ' + input.value);
+	} catch (err) {
+		// console.log('Oops, unable to copy');
+	}
+	document.body.removeChild(input);
 }
